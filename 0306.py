e70ee98b2ce507dd80d4cc9d2fe4638792b54bf9
opop1458 <48243191+opop1458@users.noreply.github.com>
2019-03-06 15:11:40 +0800

    Update 0306.py

--- 0306.py
+++ 0306.py
@@ -5,6 +5,11 @@ Created on Wed Mar  6 14:55:14 2019
 @author: admin
 """
 #%%
+
+for counter, item in enumerate(['apple', 'pear', 'apricot', 'cherry', 'peach']):
+    print(counter, item)
+#%%
+    
 j = 0
 for item  in ['apple', 'pear', 'apricot', 'cherry', 'peach']:
     print(j, item)
