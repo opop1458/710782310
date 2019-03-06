3b5b68409f7b0c66a258436c47256560cb0ec0c9
opop1458 <48243191+opop1458@users.noreply.github.com>
2019-03-06 15:03:15 +0800

    Create 0306.py

---
+++ 0306.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 14:55:14 2019
+
+@author: admin
+"""
+#%%
+j = 0
+for item  in ['apple', 'pear', 'apricot', 'cherry', 'peach']:
+    print(j, item)
+    j += 1
+    
