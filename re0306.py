61a29e6f3e670fc53b78d23f1ec1ef1efb2e1d75
opop1458 <48243191+opop1458@users.noreply.github.com>
2019-03-06 15:25:30 +0800

    Create re0306.py

---
+++ re0306.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 14:55:14 2019
+
+@author: admin
+"""
+#%%
+
+for counter, item in enumerate(['apple', 'pear', 'apricot', 'cherry', 'peach']):
+    print(counter, item)
+#%%
+    
+j = 0
+for item  in ['apple', 'pear', 'apricot', 'cherry', 'peach','banana']:
+    print(j, item)
+    j += 1
+    
